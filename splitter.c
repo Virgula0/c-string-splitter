be041f448f37fc776f810772affb131ffb34e79c
Angelo Rosa <angelo.rosa@protonmail.com>
2020-08-30 12:29:52 +0200

    Update splitter.c

--- splitter.c
+++ splitter.c
@@ -1,23 +1,18 @@
 #include "splitter.h"
 
-enum bool {
-  FALSE,
-  TRUE
-};
+enum bool { FALSE, TRUE };
 
 typedef enum bool boolean;
 
-boolean checkAllocated(char * pointer);
-int countWordsOccurrences(char * string, char separator);
-char ** getAllocatedSpace(int number, char * string);
-char ** split(char * string, char separator);
+boolean checkAllocated(char *pointer);
+int countWordsOccurrences(char *string, char separator);
+char **getAllocatedSpace(int number, char *string);
+char **split(char *string, char separator);
 
-boolean checkAllocated(char * pointer) {
-  return pointer == NULL ? FALSE : TRUE;
-}
+boolean checkAllocated(char *pointer) { return pointer == NULL ? FALSE : TRUE; }
 
-int countWordsOccurrences(char * string, char separator) {
-  char * z = malloc(sizeof(char) * strlen(string) + 1);
+int countWordsOccurrences(char *string, char separator) {
+  char *z = malloc(sizeof(char) * strlen(string) + 1);
   if (!checkAllocated(z)) {
     perror("countWordsOccurences");
     exit(EXIT_FAILURE);
@@ -25,52 +20,62 @@ int countWordsOccurrences(char * string, char separator) {
   strncpy(z, string, strlen(string));
   int number = 0;
   for (int i = 0; i < strlen(string); i++) {
-    char c = * z++;
-    if (c == separator || * z == '\0')
+    char c = *z++;
+    if (c == separator || *z == '\0')
       ++number;
+  }
+  if (z) {
+    for (int i = 0; i < strlen(string); i++)
+      z--;
+    free(z);
   }
   return number;
 }
 
-char ** getAllocatedSpace(int number, char * string) {
-  char ** strings = malloc(strlen(string) * sizeof * strings);
+char **getAllocatedSpace(int number, char *string) {
+  char **strings = malloc(strlen(string) * sizeof *strings);
   for (int i = 0; i < number; i++) {
-    strings[i] = malloc(strlen(string) * sizeof * strings);
+    strings[i] = malloc(strlen(string) * sizeof *strings);
   }
   return strings;
 }
 
-char ** split(char * string, char separator) {
+char **split(char *string, char separator) {
   int count = 0;
   int inc = 0;
   int number = countWordsOccurrences(string, separator);
   int values[number];
-  char * p = malloc(sizeof(char) * strlen(string) + 1);
+  char *p = malloc(sizeof(char) * strlen(string) + 1);
   if (!checkAllocated(p)) {
     perror("countWordsOccurences");
     exit(EXIT_FAILURE);
   }
   strncpy(p, string, strlen(string));
-  char ** strings = getAllocatedSpace(number, string);
-  if (!checkAllocated( * strings) || !checkAllocated( & ( ** strings))) {
+  char **strings = getAllocatedSpace(number, string);
+  if (!checkAllocated(*strings) || !checkAllocated(&(**strings))) {
     perror("countWordsOccurences");
     exit(EXIT_FAILURE);
   }
-  while ( * p) {
-    char current = * p++;
-    ( * strings[count]++) = current;
+  while (*p) {
+    char current = *p++;
+    (*strings[count]++) = current;
     inc++;
-    if ( * p == separator) {
+    if (*p == separator) {
       *(++strings[count]) = '\0';
       values[count] = inc + 1;
       count++;
       inc = 0;
       p++;
     }
-    if ( * p == '\0') {
+    if (*p == '\0') {
       *(++strings[count]) = '\0';
       values[count] = inc + 1;
     }
+  }
+  if (p) {
+    for (int i = 0; i < strlen(string); i++)
+      p--;
+    free(p);
   }
   for (int i = 0; i < number; i++) {
     for (int j = 0; j < values[i]; j++)
