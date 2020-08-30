a723383e7f67462916ac6ac167c5e4e9ca8e80df
Angelo Rosa <angelo.rosa@protonmail.com>
2020-08-30 12:35:09 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,13 @@ int main(){
 	char *string = "this|is|a|long|string|which|needs|to|be|splitted\n";
 	char ** strings = split(string,'|');
 	int count;
+	printf("\nOriginal one: %s",string);
+	printf("Splitted:");
 	while (*strings){
 		printf(" %s",*strings++);
 		count++;
 	}
+	printf("Original one not touched: %s\n",string);
 
 	//remember to deallocate once finished
 	if (strings){
